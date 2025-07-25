79b9a984ea1ec18bd52fa40ab82a5e63dfa532ca
Istiaque Ahmed <istiak6000@gmail.com>
2025-07-25 12:20:52 +0600

    Revert "modular code"
    This reverts commit 9b07210607a772c8ea564804b37b951c29e79259.

--- __init__.py
+++
@@ -1,5 +0,0 @@
-"""Virtual Fitness Coach Chat Application Package."""
-
-__version__ = "1.0.0"
-__author__ = "Your Name"
-__description__ = "A personalized AI fitness coach with chat history persistence"
